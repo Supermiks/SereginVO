eac2566b9aabbc8dc7d4549b6e50fb72742e8b0f
Supermiks <supermiks@yandex.ru>
2017-09-28 23:48:08 +0300

    git

---
+++ EquationSolder/EquationSolderLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquationSolderLibrary
+{
+    public class Class1
+    {
+    }
+}
