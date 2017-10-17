21392651b3bf48ecdb6fc595e61bbdaaaa6fcdf2
Supermiks <supermiks@yandex.ru>
2017-10-17 21:24:54 +0300

    Creat Structure of CarRent App

---
+++ CarRent/CarRentLibrary/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRentLibrary
+{
+    public class Car
+    {
+        public Car(string modelName, string color, OccupationStatus occupationStatus)
+        {
+            this.modelName = modelName;
+            this.color = color;
+            this.occupationStatus = occupationStatus;
+        }
+
+        string modelName;
+        string color;
+        OccupationStatus occupationStatus; 
+
+        public override string ToString()
+        {
+            return modelName + " " + color + " " + occupationStatus;
+        }
+    }
+}
